3db0bd046127a593455b2c8a11fdf27d45a4ee91
drulli <drulli@dev.java.net>
2007-08-15 18:49:57 +0000

    Removed final fields.

--- src/main/java/hudson/plugins/util/HealthReportBuilder.java
+++ src/main/java/hudson/plugins/util/HealthReportBuilder.java
@@ -15,15 +15,15 @@ public class HealthReportBuilder implements Serializable {
     /** Unique identifier of this class. */
     private static final long serialVersionUID = 5191317904662711835L;
     /** Report health as 100% when the number of warnings is less than this value. */
-    private final int healthy;
+    private int healthy;
     /** Report health as 0% when the number of warnings is greater than this value. */
-    private final int unHealthy;
+    private int unHealthy;
     /** Determines whether to use the provided healthy thresholds. */
-    private final boolean isHealthyReportEnabled;
+    private boolean isEnabled;
     /** Name of the report. */
-    private final String reportName;
+    private String reportName;
     /** Name of a item. */
-    private final String itemName;
+    private String itemName;
 
     /**
      * Creates a new instance of <code>HealthReportBuilder</code>.
@@ -44,9 +44,9 @@ public class HealthReportBuilder implements Serializable {
     public HealthReportBuilder(final String reportName, final String itemName, final boolean isHealthyReportEnabled, final int healthy, final int unHealthy) {
         this.reportName = reportName;
         this.itemName = itemName;
-        this.isHealthyReportEnabled = isHealthyReportEnabled;
         this.healthy = healthy;
         this.unHealthy = unHealthy;
+        isEnabled = isHealthyReportEnabled;
     }
 
     /**
@@ -60,7 +60,7 @@ public class HealthReportBuilder implements Serializable {
      * @return the healthiness of a build
      */
     public HealthReport computeHealth(final int counter) {
-        if (isHealthyReportEnabled) {
+        if (isEnabled) {
             int percentage;
             if (counter < healthy) {
                 percentage = 100;
@@ -77,6 +77,96 @@ public class HealthReportBuilder implements Serializable {
         else {
             return null;
         }
+    }
+
+    /**
+     * Returns the healthy.
+     *
+     * @return the healthy
+     */
+    public final int getHealthy() {
+        return healthy;
+    }
+
+    /**
+     * Sets the healthy to the specified value.
+     *
+     * @param healthy the value to set
+     */
+    public final void setHealthy(final int healthy) {
+        this.healthy = healthy;
+    }
+
+    /**
+     * Returns the unHealthy.
+     *
+     * @return the unHealthy
+     */
+    public final int getUnHealthy() {
+        return unHealthy;
+    }
+
+    /**
+     * Sets the unHealthy to the specified value.
+     *
+     * @param unHealthy the value to set
+     */
+    public final void setUnHealthy(final int unHealthy) {
+        this.unHealthy = unHealthy;
+    }
+
+    /**
+     * Returns the isHealthyReportEnabled.
+     *
+     * @return the isHealthyReportEnabled
+     */
+    public final boolean isHealthyReportEnabled() {
+        return isEnabled;
+    }
+
+    /**
+     * Sets the isHealthyReportEnabled to the specified value.
+     *
+     * @param isHealthyReportEnabled the value to set
+     */
+    public final void setHealthyReportEnabled(final boolean isHealthyReportEnabled) {
+        isEnabled = isHealthyReportEnabled;
+    }
+
+    /**
+     * Returns the reportName.
+     *
+     * @return the reportName
+     */
+    public final String getReportName() {
+        return reportName;
+    }
+
+    /**
+     * Sets the reportName to the specified value.
+     *
+     * @param reportName the value to set
+     */
+    public final void setReportName(final String reportName) {
+        this.reportName = reportName;
+    }
+
+    /**
+     * Returns the itemName.
+     *
+     * @return the itemName
+     */
+    public final String getItemName() {
+        return itemName;
+    }
+
+    /**
+     * Sets the itemName to the specified value.
+     *
+     * @param itemName the value to set
+     */
+    public final void setItemName(final String itemName) {
+        this.itemName = itemName;
     }
 }
 
