5acb99c3c1738b04e02085b74cab991485427515
drulli <drulli@dev.java.net>
2007-08-27 20:36:16 +0000

    Added new constructor.

--- src/main/java/hudson/plugins/findbugs/util/AbortException.java
+++ src/main/java/hudson/plugins/findbugs/util/AbortException.java
@@ -8,11 +8,29 @@ public final class AbortException extends RuntimeException {
     private static final long serialVersionUID = -5897876033901702893L;
 
     /**
-     * Instantiates a new abort exception.
+     * Constructs a new exception with the specified detail message.
      *
-     * @param message the exception message
+     * @param message
+     *            the detail message (which is saved for later retrieval by the
+     *            {@link #getMessage()} method).
      */
     public AbortException(final String message) {
         super(message);
+    }
+
+    /**
+     * Constructs a new exception with the specified detail message and cause.
+     *
+     * @param message
+     *            the detail message (which is saved for later retrieval by the
+     *            {@link #getMessage()} method).
+     * @param cause
+     *            the cause (which is saved for later retrieval by the
+     *            {@link #getCause()} method). (A <tt>null</tt> value is
+     *            permitted, and indicates that the cause is nonexistent or
+     *            unknown.)
+     */
+    public AbortException(final String message, final Throwable cause) {
+        super(message, cause);
     }
 }
