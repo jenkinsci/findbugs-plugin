a098e6543aa6e3bb432b68604576b5cd99ac374f
null <null@dev.java.net>
2007-10-02 22:17:15 +0000

    This commit was manufactured by cvs2svn to create tag 'findbugs-1_11'.

---
+++ hudson/plugins/findbugs/src/main/java/hudson/plugins/findbugs/FindBugsProjectAction.java
@@ -0,0 +1,42 @@
+package hudson.plugins.findbugs;
+
+import hudson.model.Project;
+import hudson.plugins.findbugs.util.AbstractProjectAction;
+
+
+/**
+ * Entry point to visualize the FindBugs trend graph in the project screen.
+ * Drawing of the graph is delegated to the associated
+ * {@link FindBugsResultAction}.
+ *
+ * @author Ulli Hafner
+ */
+public class FindBugsProjectAction extends AbstractProjectAction<FindBugsResultAction> {
+    /** Unique identifier of this class. */
+    private static final long serialVersionUID = -654316141132780561L;
+    /** URL for this action. */
+    private static final String FINDBUGS_PROJECT_URL = "findbugs";
+    /** URL to the results of the last build. */
+    private static final String FINDBUGS_RESULTS_URL = "../lastBuild/findbugsResult";
+
+    /**
+     * Instantiates a new find bugs project action.
+     *
+     * @param project
+     *            the project that owns this action
+     */
+    public FindBugsProjectAction(final Project<?, ?> project) {
+        super(project, FindBugsResultAction.class, FindBugsDescriptor.FINDBUGS_ACTION_LOGO, FINDBUGS_RESULTS_URL);
+    }
+
+    /** {@inheritDoc} */
+    public String getDisplayName() {
+        return "FindBugs Result";
+    }
+
+    /** {@inheritDoc} */
+    public String getUrlName() {
+        return FINDBUGS_PROJECT_URL;
+    }
+}
+
