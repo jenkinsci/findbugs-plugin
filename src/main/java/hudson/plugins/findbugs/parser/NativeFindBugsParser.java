7627ee73c6c12e64c4379003ad79ce1dc4f828f8
drulli <drulli@dev.java.net>
2008-03-14 09:20:38 +0000

    Fixed FindBugs library initialization: now the root dir is specified for findbugs libraries.

--- src/main/java/hudson/plugins/findbugs/parser/NativeFindBugsParser.java
+++ src/main/java/hudson/plugins/findbugs/parser/NativeFindBugsParser.java
@@ -4,11 +4,9 @@ import hudson.plugins.findbugs.util.model.LineRange;
 import hudson.plugins.findbugs.util.model.MavenModule;
 import hudson.plugins.findbugs.util.model.Priority;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Collection;
 import java.util.Iterator;
@@ -31,9 +29,6 @@ import edu.umd.cs.findbugs.ba.SourceFinder;
  * maven-findbugs-plugin >= 1.2-SNAPSHOT).
  */
 public class NativeFindBugsParser {
-    /** Determines whether we have access to the FindBugs library messages. */
-    private static boolean isLibraryInitialized;
-
     static {
        initializeFindBugs();
     }
@@ -70,16 +65,9 @@ public class NativeFindBugsParser {
         for (BugInstance warning : bugs) {
             SourceLineAnnotation sourceLine = warning.getPrimarySourceLineAnnotation();
 
-            Bug bug;
-            if (isLibraryInitialized) {
-                bug = new Bug(getPriority(warning), getMessage(warning), getCategory(warning),
+            Bug bug = new Bug(getPriority(warning), getMessage(warning), getCategory(warning),
                         warning.getType(), sourceLine.getStartLine(), sourceLine.getEndLine(),
                         warning.getBugPattern().getDetailText());
-            }
-            else {
-                bug = new Bug(getPriority(warning), getMessage(warning), getCategory(warning),
-                        warning.getType(), sourceLine.getStartLine(), sourceLine.getEndLine());
-            }
 
             Iterator<BugAnnotation> annotationIterator = warning.annotationIterator();
             while (annotationIterator.hasNext()) {
@@ -114,21 +102,7 @@ public class NativeFindBugsParser {
      * @return the bug description message.
      */
     private String getMessage(final BugInstance warning) {
-        if (useNativeLibrary()) {
-            return warning.getMessage();
-        }
-        else {
-            return StringUtils.EMPTY;
-        }
-    }
-
-    /**
-     * Returns if we should use the native library.
-     *
-     * @return <code>true</code> if we should use the native library.
-     */
-    private boolean useNativeLibrary() {
-        return isLibraryInitialized;
+        return warning.getMessage();
     }
 
     /**
@@ -176,17 +150,12 @@ public class NativeFindBugsParser {
      * @return the category as a string
      */
     private String getCategory(final BugInstance warning) {
-        if (useNativeLibrary()) {
-            BugPattern bugPattern = warning.getBugPattern();
-            if (bugPattern == null) {
-                return "Unknown";
-            }
-            else {
-                return bugPattern.getCategory();
-            }
+        BugPattern bugPattern = warning.getBugPattern();
+        if (bugPattern == null) {
+            return "Unknown";
         }
         else {
-            return StringUtils.EMPTY;
+            return bugPattern.getCategory();
         }
     }
 
@@ -195,18 +164,10 @@ public class NativeFindBugsParser {
      */
     private static void initializeFindBugs() {
         try {
-            File findBugsJar = jarFile(DetectorFactoryCollection.class);
-            String path = findBugsJar.getCanonicalPath();
-            File core = new File(path
-                    .replace("findbugs-", "coreplugin-")
-                    .replace("findbugs/findbugs", "findbugs/coreplugin")
-                    .replace("findbugs\\findbugs", "findbugs\\coreplugin"));
-
-            DetectorFactoryCollection.rawInstance().setPluginList(new URL[] {core.toURL()});
-            isLibraryInitialized = true;
+            DetectorFactoryCollection.rawInstance().setPluginList(new URL[] {new URL("file://")});
         }
-        catch (IOException exception) { // FIXME: provide a way to safely check for the existence of the library
-            isLibraryInitialized = false;
+        catch (MalformedURLException exception) {
+            throw new IllegalArgumentException(exception);
         }
     }
 
@@ -223,76 +184,6 @@ public class NativeFindBugsParser {
         project.addSourceDir(moduleRoot + "/src/test/java");
         project.addSourceDir(moduleRoot + "/src");
         return project;
-    }
-
-    // FIXME: code is copied from Hudsons Which class
-    /**
-     * Locates the jar file that contains the given class.
-     *
-     * @throws IllegalArgumentException
-     *      if failed to determine.
-     */
-    public static File jarFile(final Class clazz) throws IOException {
-        ClassLoader cl = clazz.getClassLoader();
-        if(cl==null) {
-            cl = ClassLoader.getSystemClassLoader();
-        }
-        URL res = cl.getResource(clazz.getName().replace('.', '/') + ".class");
-        if(res==null) {
-            throw new IllegalArgumentException("Unable to locate class file for "+clazz);
-        }
-        String resURL = res.toExternalForm();
-        String originalURL = resURL;
-        if(resURL.startsWith("jar:")) {
-            resURL = resURL.substring(4, resURL.lastIndexOf('!')); // cut off jar: and the file name portion
-            return new File(decode(new URL(resURL).getPath()));
-        }
-
-        if(resURL.startsWith("file:")) {
-            // unpackaged classes
-            int n = clazz.getName().split("\\.").length; // how many slashes do wo need to cut?
-            for( ; n>0; n-- ) {
-                int idx = Math.max(resURL.lastIndexOf('/'), resURL.lastIndexOf('\\'));
-                if(idx<0) {
-                    throw new IllegalArgumentException(resURL);
-                }
-                resURL = resURL.substring(0,idx);
-            }
-
-            // won't work if res URL contains ' '
-            // return new File(new URI(null,new URL(res).toExternalForm(),null));
-            // won't work if res URL contains '%20'
-            // return new File(new URL(res).toURI());
-
-            return new File(decode(new URL(resURL).getPath()));
-        }
-
-        throw new IllegalArgumentException(originalURL);
-    }
-
-    /**
-     * Decode '%HH'.
-     */
-    private static String decode(final String s) {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        for( int i=0; i<s.length();i++ ) {
-            char ch = s.charAt(i);
-            if(ch=='%') {
-                baos.write(hexToInt(s.charAt(i+1))*16 + hexToInt(s.charAt(i+2)));
-                i+=2;
-                continue;
-            }
-            baos.write(ch);
-        }
-        try {
-            return new String(baos.toByteArray(),"UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            throw new Error(e); // impossible
-        }
-    }
-
-    private static int hexToInt(final int ch) {
-        return Character.getNumericValue(ch);
     }
 }
 
