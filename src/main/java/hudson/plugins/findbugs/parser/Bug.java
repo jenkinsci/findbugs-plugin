6e0330b304b0463a4614a5d59d00b3e09ba078a3
drulli <drulli@dev.java.net>
2008-08-30 21:20:57 +0000

    Fixed equals.

--- src/main/java/hudson/plugins/findbugs/parser/Bug.java
+++ src/main/java/hudson/plugins/findbugs/parser/Bug.java
@@ -145,6 +145,9 @@ public class Bug extends AbstractAnnotation {
         if (this == obj) {
             return true;
         }
+        if (obj == null) {
+            return false;
+        }
         if (getClass() != obj.getClass()) {
             return false;
         }
