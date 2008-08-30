25477e249ebd2077aa87050e588c0c825d4074ac
drulli <drulli@dev.java.net>
2008-08-30 16:03:20 +0000

    [FIXED HUDSON-1727] Fixed short filename computation if there is no path segment in the filename.

--- src/test/java/hudson/plugins/findbugs/parser/BugsDifferencerTest.java
+++ src/test/java/hudson/plugins/findbugs/parser/BugsDifferencerTest.java
@@ -13,9 +13,10 @@ import org.apache.commons.lang.builder.HashCodeBuilder;
 public class BugsDifferencerTest extends AnnotationDifferencerTest {
     /** {@inheritDoc} */
     @Override
-    public FileAnnotation createAnnotation(final Priority priority, final String message, final String category,
+    public FileAnnotation createAnnotation(final String fileName, final Priority priority, final String message, final String category,
             final String type, final int start, final int end) {
         Bug bug = new Bug(priority, message, message, message, start, end);
+        bug.setFileName(fileName);
         bug.setInstanceHash(String.valueOf(new HashCodeBuilder().append(priority).append(message).append(category).append(type).append(start).append(end).toHashCode()));
         return bug;
     }
