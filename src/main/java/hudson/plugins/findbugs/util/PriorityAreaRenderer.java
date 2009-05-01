f698996ad9a9bcbd13838b31c8598eb0359ed485
drulli <drulli@dev.java.net>
2009-05-01 16:30:25 +0000

    Improved trend graph: - added option to control the number of considered builds (total and by day) - use default size in graph - trend links follow the selected category (priority or new vs. fixed)

--- src/main/java/hudson/plugins/findbugs/util/PriorityAreaRenderer.java
+++ src/main/java/hudson/plugins/findbugs/util/PriorityAreaRenderer.java
@@ -1,46 +1,51 @@
 package hudson.plugins.findbugs.util;
 
+import hudson.plugins.findbugs.util.model.Priority;
+
 import org.jfree.data.category.CategoryDataset;
 
 /**
  * Renderer that provides direct access to the individual results of a build via
  * links. The renderer also displays tooltips for each selected build.
  * <ul>
- * <li>The tooltip is computed per column (i.e., per build) and row (i.e.,
- * number of warnings) and shows the number of new or fixed annotations for this
- * build.</li>
+ * <li>The tooltip is computed per column (i.e., per build) and row (i.e., priority) and shows the
+ * number of annotations of the selected priority for this build.</li>
  * <li>The link is also computed per column and links to the results for this
  * build.</li>
  * </ul>
  *
  * @author Ulli Hafner
  */
-public class NewVersusFixedAreaRenderer extends AbstractBoxRenderer {
+public class PriorityAreaRenderer extends AbstractAreaRenderer {
     /** Unique identifier of this class. */
-    private static final long serialVersionUID = -2356989151327991247L;
+    private static final long serialVersionUID = -4683951507836348304L;
 
     /**
-     * Creates a new instance of {@link NewVersusFixedAreaRenderer}.
+     * Creates a new instance of {@link PriorityAreaRenderer}.
      *
      * @param url
      *            base URL of the graph links
      * @param toolTipProvider
      *            tooltip provider for the clickable map
      */
-    public NewVersusFixedAreaRenderer(final String url, final ToolTipProvider toolTipProvider) {
+    public PriorityAreaRenderer(final String url, final ToolTipProvider toolTipProvider) {
         super(url, toolTipProvider);
     }
 
     /** {@inheritDoc} */
+    @Override
     public String generateToolTip(final CategoryDataset dataset, final int row, final int column) {
         StringBuilder tooltip = new StringBuilder();
         tooltip.append(getToolTipBuilder().getTooltip(dataset.getValue(row, column).intValue()));
         tooltip.append(" ");
-        if (row == 1) {
-            tooltip.append(Messages.Trend_Fixed());
+        if (row == 2) {
+            tooltip.append(Messages.Trend_PriorityHigh());
+        }
+        else if (row == 1) {
+            tooltip.append(Messages.Trend_PriorityNormal());
         }
         else {
-            tooltip.append(Messages.Trend_New());
+            tooltip.append(Messages.Trend_PriorityLow());
         }
         return tooltip.toString();
     }
@@ -48,11 +53,14 @@ public class NewVersusFixedAreaRenderer extends AbstractBoxRenderer {
     /** {@inheritDoc} */
     @Override
     protected String getDetailUrl(final int row) {
-        if (row == 1) {
-            return "fixed";
+        if (row == 0) {
+            return Priority.LOW.name();
+        }
+        else if (row == 1) {
+            return Priority.NORMAL.name();
         }
         else {
-            return "new";
+            return Priority.HIGH.name();
         }
     }
 }
