facbbd97c634a370ff91022220fff36515a1682a
drulli <drulli@dev.java.net>
2010-03-04 23:46:13 +0000

    Added high score message for successul builds.

--- src/test/java/hudson/plugins/findbugs/FindBugsResultTest.java
+++ src/test/java/hudson/plugins/findbugs/FindBugsResultTest.java
@@ -1,9 +1,7 @@
 package hudson.plugins.findbugs;
 
-import static junit.framework.Assert.*;
 import hudson.model.AbstractBuild;
 import hudson.plugins.analysis.core.BuildHistory;
-import hudson.plugins.analysis.core.BuildResult;
 import hudson.plugins.analysis.core.ParserResult;
 import hudson.plugins.analysis.test.BuildResultTest;
 
@@ -15,32 +13,6 @@ public class FindBugsResultTest extends BuildResultTest<FindBugsResult> {
     @Override
     protected FindBugsResult createBuildResult(final AbstractBuild<?, ?> build, final ParserResult project, final BuildHistory history) {
         return new FindBugsResult(build, null, project, history);
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    protected void verifyHighScoreMessage(final int expectedZeroWarningsBuildNumber, final boolean expectedIsNewHighScore, final long expectedHighScore, final long gap, final FindBugsResult result) {
-        if (result.hasNoAnnotations() && result.getDelta() == 0) {
-            assertTrue(result.getDetails().contains(Messages.FindBugs_ResultAction_NoWarningsSince(expectedZeroWarningsBuildNumber)));
-            if (expectedIsNewHighScore) {
-                long days = BuildResult.getDays(expectedHighScore);
-                if (days == 1) {
-                    assertTrue(result.getDetails().contains(Messages.FindBugs_ResultAction_OneHighScore()));
-                }
-                else {
-                    assertTrue(result.getDetails().contains(Messages.FindBugs_ResultAction_MultipleHighScore(days)));
-                }
-            }
-            else {
-                long days = BuildResult.getDays(gap);
-                if (days == 1) {
-                    assertTrue(result.getDetails().contains(Messages.FindBugs_ResultAction_OneNoHighScore()));
-                }
-                else {
-                    assertTrue(result.getDetails().contains(Messages.FindBugs_ResultAction_MultipleNoHighScore(days)));
-                }
-            }
-        }
     }
 }
 
