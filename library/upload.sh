ddbcd0b5f77c9b97bad795e57b8a1518efe79496
drulli <drulli@dev.java.net>
2010-09-11 09:04:15 +0000

    Added scripts to upload a new findbugs library.

---
+++ library/upload.sh
@@ -0,0 +1,2 @@
+mvn deploy:deploy-file -Durl=java-net:/maven2-repository/trunk/repository/ -DrepositoryId=maven2-repository.dev.java.net -Dfile=findbugs-full-1.3.10-dev-20100910.jar -DpomFile=upload-pom.xml
+
