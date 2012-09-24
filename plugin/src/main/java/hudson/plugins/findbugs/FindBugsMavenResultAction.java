a51b7d2b2077413b3177b38f2a445c391487cbf0
Ulli Hafner <ullrich.hafner@gmail.com>
2012-09-24 23:32:13 +0200

    Fixed NPE.

--- plugin/src/main/java/hudson/plugins/findbugs/FindBugsMavenResultAction.java
+++ plugin/src/main/java/hudson/plugins/findbugs/FindBugsMavenResultAction.java
@@ -9,8 +9,10 @@ import hudson.maven.MavenModule;
 import hudson.maven.MavenModuleSet;
 import hudson.maven.MavenModuleSetBuild;
 import hudson.model.Action;
+import hudson.model.AbstractBuild;
 import hudson.plugins.analysis.core.HealthDescriptor;
 import hudson.plugins.analysis.core.MavenResultAction;
+import hudson.plugins.analysis.core.ParserResult;
 
 import java.util.List;
 import java.util.Map;
@@ -24,23 +26,6 @@ import java.util.Map;
  */
 public class FindBugsMavenResultAction extends MavenResultAction<FindBugsResult> {
     /**
-     * Creates a new instance of {@link FindBugsMavenResultAction}. This instance
-     * will have no result set in the beginning. The result will be set
-     * successively after each of the modules are build.
-     *
-     * @param owner
-     *            the associated build of this action
-     * @param healthDescriptor
-     *            health descriptor to use
-     * @param defaultEncoding
-     *            the default encoding to be used when reading and parsing files
-     */
-    public FindBugsMavenResultAction(final MavenModuleSetBuild owner, final HealthDescriptor healthDescriptor,
-            final String defaultEncoding) {
-        super(new FindBugsResultAction(owner, healthDescriptor), defaultEncoding, "FINDBUGS");
-    }
-
-    /**
      * Creates a new instance of {@link FindBugsMavenResultAction}.
      *
      * @param owner
@@ -52,14 +37,15 @@ public class FindBugsMavenResultAction extends MavenResultAction<FindBugsResult>
      * @param result
      *            the result in this build
      */
-    public FindBugsMavenResultAction(final MavenBuild owner, final HealthDescriptor healthDescriptor,
+    public FindBugsMavenResultAction(final AbstractBuild<?, ?> owner, final HealthDescriptor healthDescriptor,
             final String defaultEncoding, final FindBugsResult result) {
         super(new FindBugsResultAction(owner, healthDescriptor, result), defaultEncoding, "FINDBUGS");
     }
 
     /** {@inheritDoc} */
     public MavenAggregatedReport createAggregatedAction(final MavenModuleSetBuild build, final Map<MavenModule, List<MavenBuild>> moduleBuilds) {
-        return new FindBugsMavenResultAction(build, getHealthDescriptor(), getDisplayName());
+        return new FindBugsMavenResultAction(build, getHealthDescriptor(), getDisplayName(),
+                new FindBugsResult(build, getDefaultEncoding(), new ParserResult()));
     }
 
     /** {@inheritDoc} */
