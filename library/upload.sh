1ba7db7e2eecfe750f4a50cd8d1ba3191ac01132
Ulli Hafner <ullrich.hafner@gmail.com>
2014-03-08 12:10:26 +0100

    [JENKINS-21256] Integrate dom4j into shaded library.

--- library/upload.sh
+++ library/upload.sh
@@ -1,2 +1,2 @@
-mvn deploy:deploy-file -Durl=http://maven.hudson-labs.org:8081/content/repositories/releases/ -DrepositoryId=java.net-m2-repository -Dfile=findbugs-full-2.0.0-dev-20101217.jar -DpomFile=upload-pom.xml
+mvn deploy:deploy-file -Durl=http://maven.jenkins-ci.org:8081/content/repositories/releases/ -DrepositoryId=java.net-m2-repository -Dfile=findbugs-full-2.0.0-dev-20101217.jar -DpomFile=upload-pom.xml
 
